9eee981a8b5ff57c342e75359d6193944dc5ff06
Khairul Hakimi <148405244+Kerol04@users.noreply.github.com>
2025-11-14 10:17:58 +0800

    Update auth_services.dart

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,6 +1,38 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
+
+  // Firebase Authentication instance
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  // Firestore instance
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Function to handle user login
+  Future<String?> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      // Sign in the user using Firebase Authentication
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+
+      // Fetch the user's role from Firestore to determine access level
+      DocumentSnapshot userDoc = await _firestore
+          .collection('users')
+          .doc(userCredential.user!.uid)
+          .get();
+
+      return userDoc['role']; // Return the user's role (Admin/User)
+    } catch (e) {
+      return e.toString(); // Error: return the exception message
+    }
+  }
+
   static Future<void> setLoginStatus(bool status) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isLoggedIn', status);
